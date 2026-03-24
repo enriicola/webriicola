55af2b5df7b24506e38daaa8731d6f0c69aedfd2
Enrico Pezzano <s4825087@studenti.unige.it>
2026-03-24 12:37:02 +0100

    Update title in index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ usort($projects, function($a, $b) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>web + enriicola = webriicola</title>
+    <title>webriicola</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="style.css">
 </head>
